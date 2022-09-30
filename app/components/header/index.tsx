19eeeba5dc8f736e591e13bf3b2c5a4c3cb81e9b
Harish <harish.mahamure@bfhl.in>
2022-09-30 20:04:51 +0530

    updates

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,19 +1,40 @@
-import { NativeStackHeaderProps } from '@react-navigation/native-stack/lib/typescript/src/types'
 import React from 'react'
-import { View } from 'react-native'
-import { lightWhite, white } from '../../constants/colors'
+import { TouchableOpacity, View } from 'react-native'
+import { NativeStackHeaderProps } from '@react-navigation/native-stack/lib/typescript/src/types'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { Badge } from 'react-native-paper'
+import { black, white } from '../../constants/colors'
 import Text from '../Text'
 import styles from './styles'
+import { useNavigation } from '@react-navigation/native'
+
 const Header = (props: NativeStackHeaderProps): React.ReactNode => {
   const { route } = props
+  const { canGoBack, goBack } = useNavigation()
   return (
         <View style={styles.container}>
             <Text size={20} color={white} weight='bold'>
-              {route.name}
+            {canGoBack() && (
+              <TouchableOpacity style={{ paddingHorizontal: 10 }} onPress={goBack}>
+                <Ionicons name='md-return-down-back' size={20}/>
+              </TouchableOpacity>
+            )}
+            {route.name}
             </Text>
-            <Text color={lightWhite} >
-              {route.name}
-            </Text>
+            <View style={styles.cartContainer}>
+            {(route.name.toLowerCase() !== 'checkout') && (
+              <>
+                <TouchableOpacity disabled={true}>
+                  <Ionicons name='cart-sharp' size={32} color={white}/>
+                  <View style={styles.badge}>
+                    <Badge size={20} style={{ backgroundColor: white, color: black }}>
+                      2
+                    </Badge>
+                  </View>
+                </TouchableOpacity>
+              </>
+            )}
+            </View>
         </View>
   )
 }
--- app/components/header/styles.ts
+++ app/components/header/styles.ts
@@ -1,7 +1,12 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-  container: { padding: 10, backgroundColor: '#ff00a1', flexDirection: 'row', justifyContent: 'space-between' }
+  container: { height: 60, padding: 10, backgroundColor: '#ff00a1', flexDirection: 'row', justifyContent: 'space-between' },
+  cartContainer: {
+    position: 'relative'
+  },
+  badge:
+    { position: 'absolute', bottom: 25, left: 20 }
 })
 
 export default styles
