fd1644911a0c30ac0a865dacb6c76352787241e5
Harish <harish.mahamure@bfhl.in>
2022-10-02 08:53:38 +0530

    final changes other changes pending as travelling

--- app/modules/checkout-page/index.tsx
+++ app/modules/checkout-page/index.tsx
@@ -1,12 +1,12 @@
-import { FlatList, TouchableOpacity, View } from 'react-native'
+import { FlatList, ScrollView, TouchableOpacity, View } from 'react-native'
 import React, { useEffect } from 'react'
 import { Header, Text } from '../../components'
 import { useAppDispatch, useAppSelector } from '../../components/hooks'
 import { black, primary } from '../../constants/colors'
-import { incrementProductCartCount, decrementProductCartCount } from '../home-page/redux/reducer'
+import { incrementProductCartCount, decrementProductCartCount, ICart } from '../home-page/redux/reducer'
 import { useNavigation } from '@react-navigation/native'
 const CheckoutPage = (): JSX.Element => {
-  const { cartItems } = useAppSelector(st => st.product)
+  const { cartItems }: { cartItems: ICart[] } = useAppSelector(st => st.product)
   const dispatch = useAppDispatch()
   const { navigate } = useNavigation()
   useEffect(() => {
@@ -15,15 +15,24 @@ const CheckoutPage = (): JSX.Element => {
       navigate('Home')
     }
   }, [cartItems.length])
+
+  const total = cartItems.reduce((initial, curr) => {
+    return initial + (Number(curr.price) * Number(curr.count))
+  }, 0)
+
   return (
-    <View>
-      <Header cartItems={cartItems}/>
         <FlatList
         data={cartItems}
+        ListHeaderComponent={<Header cartItems={cartItems}/>}
+        ListFooterComponent={() => (
+        <Text size={24} weight="800">
+          Your total is {total}
+        </Text>
+        )}
         renderItem={({ item }) => {
           const { name, id, count } = item
           return (
-            <View style={{ flex: 1, paddingHorizontal: 20 }}>
+            <View style={{ flex: 1, paddingHorizontal: 20, marginHorizontal: 20 }}>
               <Text size={20} weight="600" color={black}>
                 {name}
               </Text>
@@ -45,7 +54,7 @@ const CheckoutPage = (): JSX.Element => {
           )
         }}
         />
-    </View>
+
   )
 }
 
